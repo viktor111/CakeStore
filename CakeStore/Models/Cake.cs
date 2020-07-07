83881f5764bebe273f5c988c00a07260e3671cf3
Viktor Draganov <swifrorlilko@gmail.com>
2020-07-07 22:48:32 +0300

    initial

---
+++ CakeStore/Models/Cake.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CakeStore.Models
+{
+    public class Cake
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public double Price { get; set; }
+
+        public int PiecesCount { get; set; }
+
+    }
+}
